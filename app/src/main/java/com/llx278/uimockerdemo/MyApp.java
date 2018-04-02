3f20484e17300d2c2ebb29a75890e39f5ae63cc9
Tim <luili16@126.com>
2018-04-02 18:26:00 +0800

    add

--- app/src/main/java/com/llx278/uimockerdemo/MyApp.java
+++ app/src/main/java/com/llx278/uimockerdemo/MyApp.java
@@ -1,11 +1,8 @@
 package com.llx278.uimockerdemo;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.llx278.uimocker2.Solo;
-import com.tencent.smtt.sdk.QbSdk;
-
 /**
  * Created by llx on 2018/2/22.
  */
